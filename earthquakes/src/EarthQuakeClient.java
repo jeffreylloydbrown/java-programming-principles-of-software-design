22776263a77d659fa1e4b8b4e830ffb2da675cee
Brown <jeffrey.l.brown@thomsonreuters.com>
2018-03-11 15:25:25 -0500

    implement filterByDistanceFrom() and closeToMe() with tests.

--- earthquakes/src/EarthQuakeClient.java
+++ earthquakes/src/EarthQuakeClient.java
@@ -17,11 +17,15 @@ public class EarthQuakeClient {
         return answer;
     }
 
-    public ArrayList<QuakeEntry> filterByDistanceFrom(ArrayList<QuakeEntry> quakeData,
-    double distMax,
-    Location from) {
+    public ArrayList<QuakeEntry>
+    filterByDistanceFrom(ArrayList<QuakeEntry> quakeData, double distMax, Location from) {
         ArrayList<QuakeEntry> answer = new ArrayList<QuakeEntry>();
-        // TODO
+        if (quakeData == null || quakeData.isEmpty()) return answer;
+        if (from == null) return answer;
+
+        for (QuakeEntry qe : quakeData)
+            if (qe.getLocation().distanceTo(from) < distMax)
+                answer.add(qe);
 
         return answer;
     }
@@ -53,17 +57,23 @@ public class EarthQuakeClient {
 
     public void closeToMe(){
         EarthQuakeParser parser = new EarthQuakeParser();
-        String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        //String source = "test/data/nov20quakedata.atom";
+        String source = "test/data/nov20quakedatasmall.atom";
         ArrayList<QuakeEntry> list  = parser.read(source);
         System.out.println("read data for "+list.size()+" quakes");
 
         // This location is Durham, NC
-        Location city = new Location(35.988, -78.907);
+        //Location city = new Location(35.988, -78.907);
 
         // This location is Bridgeport, CA
-        // Location city =  new Location(38.17, -118.82);
+        Location city =  new Location(38.17, -118.82);
 
-        // TODO
+        list = filterByDistanceFrom(list, 1000000, city);
+        for (QuakeEntry qe : list) {
+            System.out.println(qe.getLocation().distanceTo(city)/1000.0 + " "+ qe.getInfo());
+        }
+        System.out.println("Found " + list.size() + " quakes that match that criteria");
     }
 
     public void createCSV(){
--- earthquakes/test/EarthQuakeClientTest.java
+++ earthquakes/test/EarthQuakeClientTest.java
@@ -25,6 +25,28 @@ class EarthQuakeClientTest {
 
     @Test
     void filterByDistanceFrom () {
+        EarthQuakeClient e = new EarthQuakeClient();
+        EarthQuakeParser parser = new EarthQuakeParser();
+        String source = "test/data/nov20quakedatasmall.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        assertEquals(25, list.size());
+
+        Location durham = new Location(35.988, -78.907);
+        Location bridgeport = new Location(38.17, -118.82);
+
+        // check for null safety
+        assertEquals(0, e.filterByDistanceFrom(new ArrayList<QuakeEntry>(), 0.0, durham).size());
+        assertEquals(0, e.filterByDistanceFrom(null, 0.0, bridgeport).size());
+        assertEquals(0, e.filterByDistanceFrom(list, 0.0, null).size());
+
+        // nothing should be a negative distance
+        assertEquals(0, e.filterByDistanceFrom(list, -0.1, durham).size());
+        // everything should be within 1,000,000,000 m
+        assertEquals(25, e.filterByDistanceFrom(list, 1000000000.0, durham).size());
+
+        // from assignment
+        assertEquals(0, e.filterByDistanceFrom(list, 1000000, durham).size());
+        assertEquals(7, e.filterByDistanceFrom(list, 1000000, bridgeport).size());
     }
 
     @Test
@@ -34,15 +56,25 @@ class EarthQuakeClientTest {
     @Test
     void bigQuakes () {
         EarthQuakeClient e = new EarthQuakeClient();
+        System.out.println("bigQuakes()");
         e.bigQuakes();
+        System.out.println("complete\n");
     }
 
     @Test
     void closeToMe () {
+        EarthQuakeClient e = new EarthQuakeClient();
+        System.out.println("closeToMe()");
+        e.closeToMe();
+        System.out.println("complete\n");
     }
 
     @Test
     void createCSV () {
+        EarthQuakeClient e = new EarthQuakeClient();
+        System.out.println("createCSV()");
+        e.createCSV();
+        System.out.println("complete\n");
     }
 
 }
