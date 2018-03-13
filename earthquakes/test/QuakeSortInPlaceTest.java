28c5ee80d4a3a431d992348f1f6471a3c89d94e8
jeffreylloydbrown <jeff_brown2@msn.com>
2018-03-13 15:48:57 -0500

    created simple drivers for QuakeSortWithTwoArrayLists and QuakeSortInPlace

---
+++ earthquakes/test/QuakeSortInPlaceTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuakeSortInPlaceTest {
+    @Test
+    void testSort() {
+        QuakeSortInPlace qs = new QuakeSortInPlace();
+        qs.testSort();
+    }
+
+}
---
+++ earthquakes/test/QuakeSortWithTwoArrayListsTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuakeSortWithTwoArrayListsTest {
+    @Test
+    void testSort() {
+        QuakeSortWithTwoArrayLists qs = new QuakeSortWithTwoArrayLists();
+        qs.testSort();
+    }
+
+}
