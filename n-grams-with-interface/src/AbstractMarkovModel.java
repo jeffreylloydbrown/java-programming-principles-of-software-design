e4031464fa9119314008f7cf6fb3d258497e979c
Brown <jeffrey.l.brown@thomsonreuters.com>
2018-03-18 14:19:47 -0500

    copied code over from n-grams-no-interface, fixed it up for this assignment

---
+++ n-grams-with-interface/src/AbstractMarkovModel.java
@@ -0,0 +1,26 @@
+
+/**
+ * Abstract class AbstractMarkovModel - write a description of the class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+
+import java.util.*;
+
+public abstract class AbstractMarkovModel implements IMarkovModel {
+    protected String myText;
+    protected Random myRandom;
+    
+    public AbstractMarkovModel() {
+        myRandom = new Random();
+    }
+    
+    public void setTraining(String s) {
+        if (s != null && !s.isEmpty())
+            myText = s.trim();
+    }
+ 
+    abstract public String getRandomText(int numChars);
+
+}
---
+++ n-grams-with-interface/test/MarkovRunner.java
@@ -0,0 +1,95 @@
+import edu.duke.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarkovRunner {
+
+    @Test
+    public void runMarkov() {
+        FileResource fr = new FileResource();
+        String st = fr.asString();
+        st = st.replace('\n', ' ');
+
+        MarkovFour markov = new MarkovFour();
+        markov.setTraining(st);
+        for(int k=0; k < 3; k++){
+            String res = markov.getRandomText(200);
+            printOut(res);
+        }
+        //MarkovWordOne markovWord = new MarkovWordOne();
+        //runModel(markovWord, st, 200);
+    }
+
+    @Test
+    public void runMarkovZero() {
+        FileResource fr = new FileResource("../data/romeo.txt");
+        String st = fr.asString();
+        st = st.replace('\n', ' ');
+
+        MarkovZero markov = new MarkovZero();
+        markov.setTraining(st);
+        markov.setRandom(101);
+        for(int k=0; k < 1; k++){
+            String res = markov.getRandomText(200);
+            printOut(res);
+        }
+    }
+
+    @Test
+    public void runMarkovOne() {
+        FileResource fr = new FileResource("../data/confucius.txt");
+        String st = fr.asString();
+        st = st.replace('\n', ' ');
+
+        MarkovOne markov = new MarkovOne();
+        markov.setTraining(st);
+        markov.setRandom(42);
+        String check = markov.getRandomText(200);
+        assertTrue(check.startsWith("nd are,  Prevedowalvism n thastsour tr ndsang  heag ti. the ffinthe"));
+        printOut(check);
+    }
+
+    @Test
+    public void runMarkovFour() {
+        FileResource fr = new FileResource("../data/confucius.txt");
+        String st = fr.asString();
+        st = st.replace('\n', ' ');
+
+        MarkovFour markov = new MarkovFour();
+        markov.setTraining(st);
+        markov.setRandom(25);
+        String check = markov.getRandomText(200);
+        assertTrue(check.startsWith("ouses the people Minister said the that a many Project of it"));
+        printOut(check);
+    }
+
+    @Test
+    public void runMarkovModel() {
+        FileResource fr = new FileResource("../data/confucius.txt");
+        String st = fr.asString();
+        st = st.replace('\n', ' ');
+
+        MarkovModel markov = new MarkovModel(6);
+        markov.setTraining(st);
+        markov.setRandom(38);
+        String check = markov.getRandomText(200);
+        assertTrue(check.startsWith("sters I could thrice before downloading, and his lord, might"));
+        printOut(check);
+    }
+
+    private void printOut(String s){
+        String[] words = s.split("\\s+");
+        int psize = 0;
+        System.out.println("----------------------------------");
+        for(int k=0; k < words.length; k++){
+            System.out.print(words[k]+ " ");
+            psize += words[k].length() + 1;
+            if (psize > 60) {
+                System.out.println();
+                psize = 0;
+            }
+        }
+        System.out.println("\n----------------------------------");
+    }
+
+}
