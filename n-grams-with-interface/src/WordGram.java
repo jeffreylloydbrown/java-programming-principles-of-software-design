7499428116805dd06c7907b9624fc99f1438426f
jeffreylloydbrown <jeff_brown2@msn.com>
2018-03-22 12:39:40 -0500

    implemented WordGram, but ran out of time to get shiftAdd tests done.

--- n-grams-with-interface/src/WordGram.java
+++ n-grams-with-interface/src/WordGram.java
@@ -1,44 +1,68 @@
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
+import java.security.InvalidParameterException;
 
 public class WordGram {
     private String[] myWords;
     private int myHash;
 
     public WordGram(String[] source, int start, int size) {
+        if (source == null || source.length == 0)
+            throw new InvalidParameterException("`source` is null or empty");
+        if (start < 0 || start > source.length)
+            throw new InvalidParameterException("`source` of size "+source.length+" is too small for index `start`: "+start);
+        if (size <= 0)
+            throw new InvalidParameterException("size must be positive: "+size);
+
         myWords = new String[size];
         System.arraycopy(source, start, myWords, 0, size);
     }
 
     public String wordAt(int index) {
         if (index < 0 || index >= myWords.length) {
-            throw new IndexOutOfBoundsException("bad index in wordAt "+index);
+            throw new IndexOutOfBoundsException("bad index in wordAt: "+index);
         }
         return myWords[index];
     }
 
     public int length(){
-        // TODO: Complete this method
-        return 0;
+        return myWords.length;
     }
 
     public String toString(){
-        String ret = "";
-        // TODO: Complete this method
-
-        return ret.trim();
+        StringBuilder sb = new StringBuilder();
+        for (String word : myWords)
+            sb.append(word+" ");
+        // remember to remove the trailing space.
+        return sb.toString().trim();
     }
 
     public boolean equals(Object o) {
         WordGram other = (WordGram) o;
-        // TODO: Complete this method
+        if (myWords.length != other.length()) return false;
+        // same size, so compare contents.
+        for (int k = 0; k < myWords.length; k++)
+            if (false == myWords[k].equals(other.myWords[k]))
+                return false;
+        // all words match if we get here, so they are equal
         return true;
 
     }
 
-    public WordGram shiftAdd(String word) {	
+    public WordGram shiftAdd(String word) {
+        // myWords.length is guaranteed to be > 0 by the constructor.
         WordGram out = new WordGram(myWords, 0, myWords.length);
+
+        // decided to allow a word with no content to slide in, as an
+        // empty string.  No NULLS!  Don't have to test here for empty then.
+        if (word == null) word = "";
+
         // shift all words one towards 0 and add word at the end. 
         // you lose the first word
-        // TODO: Complete this method
+        for (int k = 0; k < out.myWords.length - 1; k++)
+            out.myWords[k] = out.myWords[k+1];
+        out.myWords[out.myWords.length-1] = word;
+
         return out;
     }
 
--- n-grams-with-interface/test/WordGramTester.java
+++ n-grams-with-interface/test/WordGramTester.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,7 +14,101 @@ class WordGramTester {
         for(int index = 0; index <= words.length - size; index += 1) {
             WordGram wg = new WordGram(words,index,size);
             System.out.println(index+"\t"+wg.length()+"\t"+wg);
+            assertTrue(source.contains(wg.toString()));
         }
+    }
+
+    private void confirmBadSource(String[] s) {
+        try {
+            WordGram wg = new WordGram(s, 0, 2);  // can't use s.length if testing s is null.
+            assertTrue(false, "exception expected, didn't throw any exception");
+        } catch (InvalidParameterException ipe) {
+            // got the expected exception, this is a pass
+        } catch (Exception e) {
+            assertTrue(false, "expected InvalidParameterException, got "+e.toString());
+        }
+    }
+
+    @Test
+    public void testBadSource() {
+        confirmBadSource(null);
+        confirmBadSource(new String[0]);
+    }
+
+    private void confirmBadStart(int start) {
+        String source = "this is a test this is a test this is a test of words";
+        String[] words = source.split("\\s+");
+        try {
+            WordGram wg = new WordGram(words, start, 2);
+            assertTrue(false, "exception expected, didn't throw any exception");
+        } catch (InvalidParameterException ipe) {
+            // got the expected exception, this is a pass
+        } catch (Exception e) {
+            assertTrue(false, "expected InvalidParameterException, got "+e.toString());
+        }
+    }
+
+    @Test
+    public void testInvalidStart () {
+        confirmBadStart(-1);
+        confirmBadStart(47);
+    }
+
+    private void confirmBadSize(int size) {
+        String source = "this is a test this is a test this is a test of words";
+        String[] words = source.split("\\s+");
+        try {
+            WordGram wg = new WordGram(words, 0, size);
+            assertTrue(false, "exception expected, didn't throw any exception");
+        } catch (InvalidParameterException ipe) {
+            // got the expected exception, this is a pass
+        } catch (Exception e) {
+            assertTrue(false, "expected InvalidParameterException, got "+e.toString());
+        }
+    }
+
+    @Test
+    public void testInvalidSize() {
+        confirmBadSize(-1);
+        confirmBadSize(0);
+    }
+
+    private void confirmBadIndex(WordGram wg, int index) {
+        try {
+            wg.wordAt(index);
+            assertTrue(false, "exception expected, didn't throw any exception");
+        } catch (IndexOutOfBoundsException ipe) {
+            // got the expected exception, this is a pass
+        } catch (Exception e) {
+            assertTrue(false, "expected IndexOutOfBoundsException, got "+e.toString());
+        }
+    }
+
+    @Test
+    public void testInvalidIndex() {
+        String source = "this is a test this is a test this is a test of words";
+        String[] words = source.split("\\s+");
+        WordGram wg = new WordGram(words, 0, 1);
+        confirmBadIndex(wg,-1);
+        confirmBadIndex(wg, wg.length());
+    }
+
+    @Test
+    public void testWordAt() {
+        String source = "this is a test this is a test this is a test of words";
+        String[] words = source.split("\\s+");
+        WordGram wg = new WordGram(words, 0, 3);
+        assertEquals("this", wg.wordAt(0));
+        assertEquals("is", wg.wordAt(1));
+        assertEquals("a", wg.wordAt(2));
+    }
+
+    @Test
+    public void testToString() {
+        String source = "this is a test this is a test this is a test of words";
+        String[] words = source.split("\\s+");
+        WordGram wg = new WordGram(words, 0, 3);
+        assertEquals("this is a", wg.toString());
     }
 
     @Test
